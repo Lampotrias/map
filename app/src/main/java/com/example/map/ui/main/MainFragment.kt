373efa6b1ef9787df377ef35d3a023d5267807ff
Mihail Mitrofanov <mitrofanov@bitrix.ru>
2022-08-23 14:10:16 +0200

    Заменил иконку для poi маркера

--- app/src/main/java/com/example/map/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/map/ui/main/MainFragment.kt
@@ -11,6 +11,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.map.R
@@ -172,6 +173,7 @@ class MainFragment : Fragment() {
 			MyMarker(binding.map).apply {
 				position = GeoPoint(it.l, it.w)
 				title = it.title
+				icon = ResourcesCompat.getDrawable(resources, R.drawable.ic_poi_marker, null)
 				setOnMarkerClickListener { marker, mapView ->
 					GlobalScope.launch {
 						withContext(Dispatchers.Main) {
